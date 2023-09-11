e87e4eecbb71952440bdd64b37daae5c8b5e57ef
Omachonu Prosper <omachonucodes@gmail.com>
2023-09-11 12:43:01 +0100

    Scafold endpoint route functions

--- app.py
+++ app.py
@@ -10,6 +10,32 @@ db = client['test']
 categories = db['categories']
 
 
+@app.route('/categories', methods=['POST', 'GET'])
+def get_create_categories():
+    return 'under construction'
+
+
+@app.route(
+    '/categories/<string:category_title>',
+    methods=['GET', 'PUT', 'DELETE']
+)
+def RUD_categories(category_title):
+    return 'under construction'
+
+
+@app.route('/categories/<string:category_title>/sub', methods=['POST'])
+def create_subcategory(category_title):
+    return 'under construction'
+
+
+@app.route(
+    '/categories/<string:category_title>/sub/<string:subcategory_title>',
+    methods=['PUT', 'DELETE']
+)
+def UD_subcategory(category_title, subcategory_title):
+    return 'under construction'
+
+
 @app.route('/')
 def index():
     return jsonify({
